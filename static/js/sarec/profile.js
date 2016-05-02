ea5fb08de0781a714291296dd40c052234cbec46
Alvaro Romero <alvaro.jrp.1994@gmail.com>
2016-05-02 15:38:03 -0600

    Validacion de campos

--- static/js/sarec/profile.js
+++ static/js/sarec/profile.js
@@ -3,55 +3,63 @@ $(document).ready(function() {
     $( "[href='/administrator/accounts/add/']" ).parent().addClass('active').parent().addClass('open');
     $( "[href='/administrator/accounts/add/']" ).parent().parent().parent().addClass('active open hsub');
 
-    $(document).on( 'submit' , "form[name='form-new-account']" ,function () {
-
-        var formData = new FormData( $( "form[name='form-new-account']" )[0] );
-
-        if( validateblank() ) {
-          $( 'div#message' ).empty().append('<div id="alert" class="alert alert-warning no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Error!</strong> Aun existen campos requeridos sin completar.</div>');
-        } else if( validatepassword() ) {
-          $( 'div#message' ).empty().append('<div id="alert" class="alert alert-warning no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Error!</strong> Los campos de contraseñas no coinciden.</div>');
-        } else if( lengthpassword() ){
-          $( 'div#message' ).empty().append('<div id="alert" class="alert alert-warning no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Error!</strong> La longitud de la contraseña, debe ser mayor a 8 caracteres.</div>');
-        } else {
-          $.ajax({
-              url : '/administrator/accounts/insert/',
-              type : 'post',
-              data : formData,
-              async : true,
-              contentType: false,
-              processData: false,
-              success: function(data) {
-                if( data.status === 'False' ) {
-                  $( 'div#message' ).empty().append('<div id="alert" class="alert alert-danger no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Error!</strong> '+ data.message +'</div>');
-                } else if( data.status === 'True' ) {
-                  $( "form[name='form-new-account']" )[0].reset();
-                  $( 'div#message' ).empty().append('<div id="alert" class="alert alert-success no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Excelente!</strong> '+ data.message +'</div>');
-                }
-              },
-              error: function (XMLHttpRequest, estado, errorS) {
-                var error = eval("(" + XMLHttpRequest.responseText + ")");
-                console.log(error.Message);
-                console.log(estado);
-                console.log(errorS);
-              },
-              complete: function (jqXHR, estado) {
-              }
-          });
-        }
-
-        return false;
+    $( " input[type='text'], input[type='email'], input[type='password'] " ).on( 'input' , function() {
+      if( $( this ).val() === '' ) {
+        $( this ).parent().parent().addClass('has-error');
+      } else {
+        $( this ).parent().parent().removeClass('has-error');
+      }
     });
 
+    $(document).on( 'submit' , "form[name='form-new-account']" ,function () {
+      var formData = new FormData( $( "form[name='form-new-account']" )[0] );
+
+      if( validateblank() ) {
+        $( 'div#message' ).empty().append('<div id="alert" class="alert alert-warning no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Error!</strong> Aun existen campos requeridos sin completar.</div>');
+      } else if( validatepassword() ) {
+        $( 'div#message' ).empty().append('<div id="alert" class="alert alert-warning no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Error!</strong> Los campos de contraseñas no coinciden.</div>');
+      } else if( lengthpassword() ){
+        $( 'div#message' ).empty().append('<div id="alert" class="alert alert-warning no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Error!</strong> La longitud de la contraseña, debe ser mayor a 8 caracteres.</div>');
+      } else {
+        $.ajax({
+            url : '/administrator/accounts/insert/',
+            type : 'post',
+            data : formData,
+            async : true,
+            contentType: false,
+            processData: false,
+            success: function(data) {
+              if( data.status === 'False' ) {
+                $( 'div#message' ).empty().append('<div id="alert" class="alert alert-danger no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Error!</strong> '+ data.message +'</div>');
+                if( 'Lo sentimos, este nombre de usuario ya ha siso registrado, por favor seleccione otro...' === data.message ){
+                  $( "input[name='username']" ).parent().parent().addClass('has-error');
+                }
+              } else if( data.status === 'True' ) {
+                $( "form[name='form-new-account']" )[0].reset();
+                $( 'div#message' ).empty().append('<div id="alert" class="alert alert-success no-margin-bottom"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button><strong>Excelente!</strong> '+ data.message +'</div>');
+              }
+            },
+            error: function (XMLHttpRequest, estado, errorS) {
+              var error = eval("(" + XMLHttpRequest.responseText + ")");
+              console.log(error.Message);
+              console.log(estado);
+              console.log(errorS);
+            },
+            complete: function (jqXHR, estado) {
+            }
+        });
+      }
+
+      return false;
+  });
 });
 
 function validateblank() {
     var validate = false;
     $(" input[type='text'], input[type='email'], input[type='password'] ").each(function(){
-      console.log( $( this ).prop('name') );
-      console.log( $( this ).val() );
-        if( $(this).val() === '' ) {
+        if( $( this ).val() === '' ) {
             validate = true;
+            $( this ).parent().parent().addClass('has-error');
         }
     });
     return validate;
@@ -61,6 +69,7 @@ function validatepassword() {
     var validate = false;
     if( $( "input[name='newpassword']" ).val() !== $( "input[name='confirmpassword']" ).val() ) {
         validate = true;
+        $(" input[type='password'] ").parent().parent().addClass('has-error');
     }
     return validate;
 }
@@ -69,6 +78,7 @@ function lengthpassword() {
     var validate = false;
     if( $( "input[name='newpassword']" ).val().length < 8 ) {
         validate = true;
+        $(" input[type='password'] ").parent().parent().addClass('has-error');
     }
     return validate;
 }
